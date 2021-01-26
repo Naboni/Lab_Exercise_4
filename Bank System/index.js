e074572dab73e8150e28197b3c1b2596686dc245
Naboni Abebe <se.naboni.abebe@gmail.com>
2021-01-27 00:15:21 +0300

    I have completed the Bank System. With all of the four functions including transfer

--- Bank System/index.js
+++ Bank System/index.js
@@ -1,7 +1,7 @@
 var person1 = {
     name : "Abe",
     acNo : 0001,
-    balance: 2000
+    balance: 90
 }
 
 var person2 = {
@@ -20,7 +20,8 @@ function deposit(){
         let mon = parseFloat(prompt("Enter how much u want to deposit: ")) 
         let newmon = person1.balance+=mon;
         alert("You have now "+ newmon);
-    } else if (acName==person2.name && acNum==person2.acNo){
+    } 
+    else if (acName==person2.name && acNum==person2.acNo){
         alert("Hello "+person2.name);
         let mon = parseFloat(prompt("Enter how much u want to deposit: ")) 
         let newmon = person2.balance+=mon;
@@ -35,15 +36,24 @@ function withdraw(){
     let acNum = parseInt(prompt("Enter ur account number: "));
     
     if (acName==person1.name && acNum==person1.acNo){
-        alert("Hello "+person1.name);
-        let mon = parseFloat(prompt("Enter how much u want to withdraw: ")) 
-        let newmon = person1.balance-=mon;
-        alert("You have now "+ newmon);
+        if (person1.balance>=100){
+            alert("Hello "+person1.name);
+            let mon = parseFloat(prompt("Enter how much u want to withdraw: ")) 
+            let newmon = person1.balance-=mon;
+            alert("You have now "+ newmon);
+        }else{
+            alert("You cant withdraw. Low amount")
+        }
+        
     } else if (acName==person2.name && acNum==person2.acNo){
-        alert("Hello "+person2.name);
-        let mon = parseFloat(prompt("Enter how much u want to withdraw: ")) 
-        let newmon = person2.balance-=mon;
-        alert("You have now "+ newmon);
+        if (person2.balance>=100){
+            alert("Hello "+person2.name);
+            let mon = parseFloat(prompt("Enter how much u want to withdraw: ")) 
+            let newmon = person2.balance-=mon;
+            alert("You have now "+ newmon);
+        }else{
+            alert("You cant withdraw. Low amount")
+        }
     }else{
         alert("Error")
     }
@@ -57,31 +67,55 @@ function balance(){
         alert("Hello "+person1.name);
         alert("You have now "+ person1.balance);
     } else if (acName==person2.name && acNum==person2.acNo){
-        alert("Hello "+person1.name);
-        alert("You have now "+ person1.balance);
+        alert("Hello "+person2.name);
+        alert("You have now "+ person2.balance);
     }else{
         alert("Error")
     }
 }
 
-// while(true){
-//     (function(){
-//         var choice = parseInt(prompt("1 for deposit.\n 2 for withdraw. \n 3 for balance. \n 4 for transfer."))
-//         if(choice == 1){
-//             deposit();
-//         }else if(choice == 2){
-//             withdraw()
-//         }else if(choice == 3){
-//             balance()
-//         }else if(choice == 4){
-//             transfer()
-//         }else{
-//             alert("You chose inappropriately.")
-//         }
+function transfer(){
+    let acName = prompt("Enter ur name: ");
+    let acNum = parseInt(prompt("Enter ur account number: "));
+    
+    if (acName==person1.name && acNum==person1.acNo){
+        alert("Hello "+person1.name);
+        let mon = parseFloat(prompt("Enter how much do u want to transfer: ")) 
+        alert("You have transferred "+ mon + " to " + person2.name);
+        person1.balance-=mon;
+        person2.balance+=mon;
+    } else if (acName==person2.name && acNum==person2.acNo){
+        alert("Hello "+person2.name);
+        let mon = parseFloat(prompt("Enter how much do u want to transfer: ")) 
+        alert("You have transferred "+ mon + " to " + person1.name);
+        person2.balance-=mon;
+        person1.balance+=mon;
+    }else{
+        alert("Error")
+    }
+}
+var x = 0;
+while(x==0){
+    (function(){
+        var choice = parseInt(prompt("1 for deposit.\n 2 for withdraw. \n 3 for balance. \n 4 for transfer. \n 5 for Exit"))
+        if(choice == 1){
+            deposit();
+        }else if(choice == 2){
+            withdraw()
+        }else if(choice == 3){
+            balance()
+        }else if(choice == 4){
+            transfer()
+        }else if(choice == 5){
+            x=1;
+        }
+        else{
+            alert("You chose inappropriately.")
+        }
         
-//     })()
-// }
+    })()
+}
 
 
-balance();
+
 
